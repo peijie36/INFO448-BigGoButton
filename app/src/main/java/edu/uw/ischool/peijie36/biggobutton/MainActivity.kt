ee60f4d56c8091076cf7c6339118bfceccb31b18
peijie36 <peijiezheng2001@gmail.com>
2024-01-29 22:13:20 -0800

    Finish creating big go button

--- app/src/main/java/edu/uw/ischool/peijie36/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/peijie36/biggobutton/MainActivity.kt
@@ -1,11 +1,36 @@
 package edu.uw.ischool.peijie36.biggobutton
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bigGoBtn = findViewById<Button>(R.id.big_go_btn)
+
+        bigGoBtn.setOnClickListener(View.OnClickListener {
+            count += 1
+            if (count > 1) {
+                bigGoBtn.setText("You have pushed me $count times")
+            } else {
+                bigGoBtn.setText("You have pushed me $count time")
+            }
+            //bigGoBtn.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            bigGoBtn.setBackgroundColor(Color.parseColor(generateRandomHexCode()))
+            bigGoBtn.setTextColor(Color.parseColor(generateRandomHexCode()))
+        })
     }
+
+    private fun generateRandomHexCode(): String {
+        val random = Random.Default
+        val color = random.nextInt(0xFFFFFF + 1)
+        return String.format("#%06X", color)
+    }
+
 }
